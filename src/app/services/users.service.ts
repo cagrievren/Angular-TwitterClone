b2e3f20802cb196135d6ac2825f6d2490e713436
Çağrı Evren <cagrievren93@gmail.com>
2019-12-26 15:06:39 +0300

    service added

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,11 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { signInInfo } from 'src/sign-in';
 @Injectable({
   providedIn: 'root'
 })
-export class UsersService {
+export class UsersService implements OnInit{
 
   users: signInInfo[] = [];
-  
+
   constructor() { }
+
+  ngOnInit() {
+    this.addSomeUser();
+  }
+  addSomeUser() {
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+    this.users.push(new signInInfo('Oğuzhan','Atasever', 'oguzhanatasever@yaani.com', '12341234'));
+
+  }
 }
