770909b7b53b944beb5f169212bed90340d860c3
Oğuzhan <oguzhanatasever93@gmail.com>
2020-01-15 16:51:45 +0300

    search

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,21 +1,22 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Post } from "src/app/post.model";
-import { Router } from "@angular/router";
-import { DataService } from "src/app/services/data.service";
-import { LoginService } from "../../services/login.service";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Post } from './../../post.model';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
+import { LoginService } from '../../services/login.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-navbar",
-  templateUrl: "./navbar.component.html",
-  styleUrls: ["./navbar.component.css"]
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
   @Input() userInfo: Post;
 
   allUsers = [];
-  searchText = "";
+  searchText = '';
   myUser;
+  myUsers = [];
 
   searchForm: FormGroup;
 
@@ -23,13 +24,13 @@ export class NavbarComponent implements OnInit {
     private router: Router,
     private dataService: DataService,
     private loginService: LoginService
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getUsers();
 
     this.searchForm = new FormGroup({
-      name: new FormControl(null, [Validators.required])
+      name: new FormControl('', [Validators.required])
     });
   }
 
@@ -41,23 +42,37 @@ export class NavbarComponent implements OnInit {
       }
     });
   }
-  searchUser() {
-    console.log(this.searchText);
+  git(user){
 
+    console.log('USER INFO');
+    console.log(this.userInfo);
+
+    // let gecici = new Post(user.name + ' ' + user.surname, user.email, '', new Date());
+
+    let gecici = {} as Post;
+    gecici.id = user.name + ' ' + user.surname;
+    gecici.author = user.email;
+    gecici.text = '';
+    gecici.time = new Date();
+
+    this.dataService.setData('user', [gecici]);
+    this.router.navigate(['/detail/user']);
+  }
+  searchUser() {
     console.log(this.allUsers);
 
     this.allUsers.forEach(element => {
-      if (element.name.indexOf(this.searchText) > 0) {
-        this.myUser = element;
+      if (element.surname.indexOf(this.searchForm.value.name) !== -1) {
+        this.myUsers.push(element);
       }
     });
-    console.log(this.myUser);
+    console.log(this.myUsers);
   }
   onDetail() {
-    console.log("onDetail");
+    console.log('onDetail');
 
-    this.dataService.setData("user", [this.userInfo]);
-    this.router.navigate(["/detail/user"]);
+    this.dataService.setData('user', [this.userInfo]);
+    this.router.navigate(['/detail/user']);
   }
   onSubmit() {
     this.searchForm.reset();
